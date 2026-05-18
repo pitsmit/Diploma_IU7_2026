f16a049fd558c2a45697425d7eb5ae309fa8fc5a
Peter <petr_smirnov_2004@mail.ru>
2026-05-19 01:16:14 +0300

    some issues

--- Notes/mydlp-front/vitest.config.ts
+++ Notes/mydlp-front/vitest.config.ts
@@ -9,6 +9,11 @@ export default mergeConfig(
       environment: 'jsdom',
       exclude: [...configDefaults.exclude, 'e2e/**'],
       root: fileURLToPath(new URL('./', import.meta.url)),
+      coverage: {
+        provider: 'v8', // или 'istanbul'
+        include: ['src/**/*.{ts,js,vue}'], // какие файлы включать в покрытие
+        exclude: ['**/*.test.ts', '**/*.spec.ts', 'e2e/**', "src/App.vue", "src/main.ts", "src/api/websocket.ts", "src/api/api.ts", "src/components/AppContainer.vue", "src/router/index.ts"],
+      },
     },
   }),
 )
